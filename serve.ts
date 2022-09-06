2c275edf299b0f94359828fabe639731eeba2d13
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2022-09-06 11:38:18 +0530

    chore: use bot.token instead

--- serve.ts
+++ serve.ts
@@ -1,10 +1,10 @@
 import { serve, webhookCallback } from "./deps.ts";
-import { bot, TOKEN } from "./bot.ts";
+import { bot } from "./bot.ts";
 
 const handleUpdate = webhookCallback(bot, "std/http");
 
 serve(async (req) => {
-  if (new URL(req.url).pathname === `/${TOKEN}` && req.method === "POST") {
+  if (new URL(req.url).pathname === `/${bot.token}` && req.method === "POST") {
     if (req.method === "POST") {
       try {
         return await handleUpdate(req);
