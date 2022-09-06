f3149ad92bd04dc0d56f91fcc3a1a1e1668e1df6
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2022-09-06 11:40:19 +0530

    chore: bump deps

--- deps.ts
+++ deps.ts
@@ -10,20 +10,20 @@ export {
   type LazySessionFlavor,
   type NextFunction,
   webhookCallback,
-} from "https://deno.land/x/grammy@v1.10.1/mod.ts";
+} from "https://deno.land/x/grammy@v1.11.0/mod.ts";
 export type {
   Message,
   ParseMode,
-} from "https://deno.land/x/grammy@v1.10.1/types.ts";
-export { freeStorage } from "https://deno.land/x/grammy_storages@v2.0.0/free/src/mod.ts";
+} from "https://deno.land/x/grammy@v1.11.0/types.ts";
+export { freeStorage } from "https://deno.land/x/grammy_storages@v2.0.1/free/src/mod.ts";
 
 // utils
 export {
   getTimeZones,
   timeZonesNames,
-} from "https://cdn.skypack.dev/@vvo/tzdb@v6.52.0?dts";
+} from "https://cdn.skypack.dev/@vvo/tzdb@v6.62.0?dts";
 // @deno-types="https://deno.land/x/fuse@v6.4.1/dist/fuse.d.ts"
 export { default as Fuse } from "https://deno.land/x/fuse@v6.4.1/dist/fuse.esm.min.js";
 
 // server (deno deploy)
-export { serve } from "https://deno.land/std@0.152.0/http/mod.ts";
+export { serve } from "https://deno.land/std@0.154.0/http/mod.ts";
